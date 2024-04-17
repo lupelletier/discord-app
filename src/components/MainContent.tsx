89d5cee9d317ccc49e78ac42c0caa826c54202fc
lupelletier <lucie.pelletier@my-digital-school.org>
2024-04-17 10:42:44 +0200

    WIP: messages are displayed at the end of conversation

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -5,9 +5,10 @@ import MessageCard from './MessageCard';
 export default function MainContent() {
   const messages = useContext(messagesContext);
   return (
-    <main className='fixed w-5/6 h-full top-0 right-0 bg-gray-800'>
-      <div className='h-full'>
-        <div className='flex flex-col w-full p-10 h-5/6 overflow-y-scroll justify-end'>
+    <main className='fixed w-5/6 h-full top-0 right-0 bg-gray-800 flex flex-col'>
+      <div className='p-4 h-full'>
+        <div className='flex flex-col h-5/6 justify-end top-0'>
+
           {messages?.messages.map((message, index) => (
             <MessageCard key={index} message={message} />
           ))}
